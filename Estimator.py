d10fb9b9a5d7d1174ea9b09a71ea6639d368c06e
Wayne Chen <jungwnr@hotmail.com>
2019-12-28 22:49:45 -0800

    Updated to a non-sequential model.  Hooray!

--- Estimator.py
+++ Estimator.py
@@ -16,116 +16,95 @@ BATCH_SIZE = 24
 
 ################################################# 
 OLD_LOSS = 100.0
+OLD_ACC = 0.0
+OLD_VLOSS = 100.0
+OLD_VACC = 0.0
 
 class VisualizerCB(tensorflow.keras.callbacks.Callback): 
     def on_epoch_end(self, epoch, logs={}): 
-        global OLD_LOSS
-        if(OLD_LOSS > logs["loss"]):
-            logger.log(str(epoch), str(logger.GREEN + str(logs) + logger.RESET))
-        else:
-            logger.log(str(epoch), str(logger.RED + str(logs) + logger.RESET))
+        global OLD_LOSS, OLD_ACC, OLD_VLOSS, OLD_VACC
+
+        l1 = logger.YELLOW if(OLD_LOSS == logs["loss"]) else logger.GREEN if(OLD_LOSS > logs["loss"]) else logger.RED
+        l2 = logger.YELLOW if(OLD_ACC == logs["accuracy"]) else logger.GREEN if(OLD_ACC < logs["accuracy"]) else logger.RED
+        l3 = logger.YELLOW if(OLD_VLOSS == logs["val_loss"]) else logger.GREEN if(OLD_VLOSS > logs["val_loss"]) else logger.RED
+        l4 = logger.YELLOW if(OLD_VACC == logs["val_accuracy"]) else logger.GREEN if(OLD_VACC < logs["val_accuracy"]) else logger.RED
+
+        # logger.log(str(epoch), str("Loss: " + l1 + str(logs["loss"]).ljust(25) + logger.RESET + " Accuracy: " + l2 + str(logs["accuracy"]).ljust(15) + logger.RESET + " Val_Loss: " + l3 + str(logs["val_loss"]).ljust(25) + logger.RESET + " Val_Accuracy: " + l4 + str(logs["val_accuracy"]).ljust(15) + logger.RESET))
+        logger.log(str(epoch), str(", " + l1 + str(logs["loss"]) + logger.RESET + ", " + l2 + str(logs["accuracy"]) + logger.RESET + ", " + l3 + str(logs["val_loss"]) + logger.RESET + ", " + l4 + str(logs["val_accuracy"]) + logger.RESET))
         OLD_LOSS = logs["loss"]
-
-class PixelMapModel(tensorflow.keras.Model):
-    def __init__(self):
-        super(PixelMapModel, self).__init__()
-
-        self.lyr = [
-            tensorflow.keras.layers.Conv2D(4, (2, 2), strides=(1, 1), activation=tensorflow.nn.relu),
-            tensorflow.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
-            tensorflow.keras.layers.Conv2DTranspose(4, (4, 4), strides=(2, 2))
-            ]
-        
-        self.lyO = [None, None, None]
-
-    def call(self, inputs):
-        self.lyO[0] = self.lyr[0](inputs)
-        self.lyO[1] = self.lyr[1](self.lyO[0])
-        self.lyO[2] = self.lyr[2](self.lyO[1])
-        return self.lyO[2]
-
-    def getResult(self, x, y, p, images):
-        for i in self.lyr:
-            try:
-                img = i.call(images[0:20]) #self.get_layer(index=i).call(images[0:20])
-                pyplot.subplot(x,y,p)
-                pyplot.imshow(img[10])
-                p += 1
-                logger.log("PixelMapModel", str(logger.GREEN + "RENDER" + logger.RESET))
-            except Exception as e:
-                logger.log("PixelMapModel", str(logger.RED + str(e) + logger.RESET))
-
-class DenseModel(tensorflow.keras.Model):
-    def __init__(self):
-        super(DenseModel, self).__init__()
-
-        self.lyr = [
-            tensorflow.keras.layers.Dense(4, activation=tensorflow.nn.softmax)
-            ]
-
-    def call(self, inputs):
-        return self.lyr[0](inputs)
-
-    def getResult(self, x, y, p, images):
-        for i in self.lyr:
-            try:
-                img = i.call(images[0:20]) #self.get_layer(index=i).call(images[0:20])
-                pyplot.subplot(x,y,p)
-                pyplot.imshow(img[10])
-                p += 1
-                logger.log("DenseModel", str(logger.GREEN + "RENDER" + logger.RESET))
-            except Exception as e:
-                logger.log("DenseModel", str(logger.RED + str(e) + logger.RESET))
-
-class MyModel(tensorflow.keras.Model):
-    def __init__(self):
-        super(MyModel, self).__init__([modelA, modelB])
-
-        self.lyr = [
-            tensorflow.keras.layers.Dense(4, activation=tensorflow.nn.softmax)
-            ]
-
-    def call(self, inputs):
-        return self.lyr[0](inputs)
-
-    def getResult(self, x, y, p, images):
-        for i in self.lyr:
-            try:
-                img = i.call(images[0:20]) #self.get_layer(index=i).call(images[0:20])
-                pyplot.subplot(x,y,p)
-                pyplot.imshow(img[10])
-                p += 1
-                logger.log("MyModel", str(logger.GREEN + "RENDER" + logger.RESET))
-            except Exception as e:
-                logger.log("MyModel", str(logger.RED + str(e) + logger.RESET))
+        OLD_ACC = logs["accuracy"]
+        OLD_VLOSS = logs["val_loss"]
+        OLD_VACC = logs["val_accuracy"]
 
 class MyModelV2(tensorflow.keras.Model):
     def __init__(self):
         super(MyModelV2, self).__init__()
 
+        self.lyr = [
+            tensorflow.keras.layers.Dense(4, activation=tensorflow.nn.relu)
+            ]
+
+        self.x = [
+            tensorflow.keras.layers.Conv2D(4, (2, 2), strides=(1, 1), activation=tensorflow.nn.relu),
+            tensorflow.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+            tensorflow.keras.layers.Conv2DTranspose(4, (4, 4), strides=(2, 2))
+            ]
+        
+        self.y = [
+            tensorflow.keras.layers.Conv2D(4, (4, 4), strides=(2, 2), activation=tensorflow.nn.relu),
+            tensorflow.keras.layers.MaxPooling2D(pool_size=(4, 4), strides=(2, 2)),
+            tensorflow.keras.layers.Conv2DTranspose(4, (22, 22), strides=(2, 2))
+            ]
+
     def call(self, inputs):
-        x = tensorflow.keras.layers.Conv2D(4, (2, 2), strides=(1, 1), activation=tensorflow.nn.relu)(inputs)
-        x = tensorflow.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
-        x = tensorflow.keras.layers.Conv2DTranspose(4, (4, 4), strides=(2, 2))(x)
-        x = tensorflow.keras.Model(inputs=inputs, outputs=x)
+        x = self.x[0](inputs)
+        x = tensorflow.nn.dropout(x, 0.3)
+        x = self.x[1](x)
+        x = self.x[2](x)
 
-        y = tensorflow.keras.layers.Dense(4, activation=tensorflow.nn.softmax)(inputs)
-        y = tensorflow.keras.Model(inputs=inputs, outputs=y)
+        y = self.y[0](inputs)
+        x = tensorflow.nn.dropout(x, 0.1)
+        y = self.y[1](y)
+        y = self.y[2](y)
 
-        combined = tensorflow.concat([x, y], 0)
+        combined = tensorflow.concat([x, y], 3)
 
-        return self.lyr[0](inputs)
+        return self.lyr[0](combined)
 
     def getResult(self, x, y, p, images):
+        for i in self.x:
+            try:
+                img = i.call(images[0:20]) #self.get_layer(index=i).call(images[0:20])
+                pyplot.subplot(x,y,p)
+                pyplot.imshow(img[10])
+                logger.log("MyModelX", str(logger.GREEN + "RENDER" + logger.RESET))
+            except Exception as e:
+                logger.log("MyModelX", str(logger.RED + str(e) + logger.RESET))
+            p += 1
+
+        p += 1
+
+        for i in self.y:
+            try:
+                img = i.call(images[0:20]) #self.get_layer(index=i).call(images[0:20])
+                pyplot.subplot(x,y,p)
+                pyplot.imshow(img[10])
+                logger.log("MyModelY", str(logger.GREEN + "RENDER" + logger.RESET))
+            except Exception as e:
+                logger.log("MyModelY", str(logger.RED + str(e) + logger.RESET))
+            p += 1
+
+        p += 1
+
         for i in self.lyr:
             try:
                 img = i.call(images[0:20]) #self.get_layer(index=i).call(images[0:20])
                 pyplot.subplot(x,y,p)
                 pyplot.imshow(img[10])
-                p += 1
-                logger.log("MyModel", str(logger.GREEN + "RENDER" + logger.RESET))
+                logger.log("MyModelZ", str(logger.GREEN + "RENDER" + logger.RESET))
             except Exception as e:
-                logger.log("MyModel", str(logger.RED + str(e) + logger.RESET))
+                logger.log("MyModelZ", str(logger.RED + str(e) + logger.RESET))
+            p += 1
 
 
 logger.log("TENSORFLOW_VERSION", str(tensorflow.__version__))
@@ -137,7 +116,7 @@ if gpus:
            tensorflow.config.experimental.set_memory_growth(gpu, True)
         tensorflow.config.experimental.set_visible_devices(gpus[0], 'GPU')
         logical_gpus = tensorflow.config.experimental.list_logical_devices('GPU')
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+        logger.log("GPU", str(len(gpus)) + " Physical GPUs, " + str(len(logical_gpus)) + " Logical GPU")
         logger.log("Physical GPU", str(len(gpus)))
         logger.log("Logical GPU", str(len(logical_gpus)))
     except RuntimeError as e:
@@ -146,7 +125,7 @@ if gpus:
 
 trainingSet = ImageConverter.ImageConverter(os.path.dirname(__file__) + "\images_training", 32, 32)
 trainingImages, trainingLabels = trainingSet.process(True)
-
+ 
 pyplot.figure(figsize=(4,2))
 pyplot.subplot(3,8,1)
 pyplot.imshow(trainingImages[0])
@@ -158,7 +137,7 @@ testImages, testLabels = testSet.process(True)
 
 # beef = tensorflow.concat([1, 2], 0)
 
-model = PixelMapModel()
+model = MyModelV2()
 #mB = DenseModel()
 #model = MyModel(mA, mB)
 
@@ -168,11 +147,12 @@ checkpoint_dir = os.path.dirname(checkpoint_path)
 train = True
 if(train == True):
     logger.log("Train", "TN")
-    #model.load_weights('data/weights')
+    model.load_weights('data/weights')
     model.compile(optimizer='adam',
               loss=tensorflow.keras.losses.Huber(),
               metrics=["accuracy"])
-    model.fit(trainingImages, trainingLabels, verbose=0, shuffle=True, epochs=5000, validation_data=(testImages, testLabels), workers=8, use_multiprocessing=True, callbacks=[VisualizerCB()])
+    model.fit(trainingImages, trainingLabels, verbose=0, shuffle=True, epochs=2000, validation_data=(testImages, testLabels), workers=8, use_multiprocessing=True, callbacks=[VisualizerCB()])
+    #model.fit(trainingImages, trainingLabels, verbose=0, shuffle=True, epochs=300, validation_data=(testImages, testLabels), workers=8, use_multiprocessing=True)
     model.save_weights("data/weights")
 else:
     model.load_weights('data/weights')
@@ -181,23 +161,18 @@ else:
               metrics=["accuracy"])
     model.build((1, 32, 32, 4))
 
-logger.log("MODEL SUMMARY", str(model.summary()))
+# logger.log("MODEL SUMMARY", str(model.summary()))
 
 prediction = model.predict(testImages[0:20], steps=42)
 
-#mA.getResult(3, 8, 8, testImages[0:20])
-#mB.getResult(3, 8, 16, testImages[0:20])
-model.getResult(3, 8, 16, testImages[0:20])
-#imm_4 = model.get_layer(index=3).call(testImages[0:20])
-#pyplot.subplot(2,8,6)
-#pyplot.imshow(imm_4[10])
+model.getResult(3, 8, 9, testImages[0:20])
 
 pIndex = 0
 
-pyplot.subplot(3,8,6)
+pyplot.subplot(3,8,22)
 pyplot.imshow(testImages[0])
 
-pyplot.subplot(3,8,7)
+pyplot.subplot(3,8,23)
 pyplot.imshow(prediction[pIndex])
 pyplot.show()
 
