5f156042b41ce29f5446d4bddadb9bf2ba87d303
Wayne Chen <jungwnr@hotmail.com>
2020-01-07 22:20:13 -0800

    Simple RNN Base

--- Sample_RNN.py
+++ Sample_RNN.py
@@ -10,13 +10,14 @@ import matplotlib.pyplot as pyplot
 
 import lib.logger.logger as logger
 
+import SineWave
 import Loader
 import ImageConverter
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-NEW = False
-TRAIN = False
+NEW = True
+TRAIN = True
 EPOCHS = 1000
 BATCH_SIZE = 128
 
@@ -38,7 +39,10 @@ class VisualizerCB(tensorflow.keras.callbacks.Callback):
         newLossDelta = OLD_LOSS - logs["loss"]
         newAccDelta = OLD_ACC - logs["accuracy"]
 
-        lossDeltaPercentage = newLossDelta / LOSS_DELTA
+        if(LOSS_DELTA == 0):
+            lossDeltaPercentage = 0
+        else:
+            lossDeltaPercentage = newLossDelta / LOSS_DELTA
 
         l1 = logger.YELLOW if(OLD_LOSS == logs["loss"]) else logger.GREEN if(OLD_LOSS > logs["loss"]) else logger.RED
         l2 = logger.YELLOW if(OLD_ACC == logs["accuracy"]) else logger.GREEN if(OLD_ACC < logs["accuracy"]) else logger.RED
@@ -67,41 +71,22 @@ class MyModelV2(tensorflow.keras.Model):
     def __init__(self):
         super(MyModelV2, self).__init__()
 
-        self.lyr = {
-            "lyr": [
+        self.lyr = [
                 tensorflow.keras.layers.Dense(32),
-                tensorflow.keras.layers.Dropout(0.5),
-                tensorflow.keras.layers.Dense(4, activation=tensorflow.nn.leaky_relu),
-                tensorflow.keras.layers.Conv2DTranspose(4, 2, strides=1)
+                tensorflow.keras.layers.Dense(28, activation=tensorflow.nn.leaky_relu),
+                tensorflow.keras.layers.Dense(6)
                 ]
-            }
-
-        self.parallelLayers = {
-            "x": [
-                tensorflow.keras.layers.Conv2D(4, 2, strides=1, activation=tensorflow.nn.softmax),
-            ],
-            "y": [
-                tensorflow.keras.layers.Conv2D(4, 2, strides=1, activation=tensorflow.nn.sigmoid),
-            ]
-        }
 
     def call(self, inputs):
         logger.log(logger.CYAN + "Model Called" + logger.RESET, "")
         tensors = []
         idx = 0
-        for t in self.parallelLayers:
-            tensors.append(self.parallelLayers[t][0](inputs))
-            for i in range(1, len(self.parallelLayers[t])):
-                tensors[idx] = self.parallelLayers[t][i](tensors[idx])
-            idx += 1
-        
-        combined = tensorflow.concat(tensors, 3)
 
         logger.log(logger.CYAN + "Model Called" + logger.RESET, "Layers Executed. Now doing final layer")
 
-        result = self.lyr["lyr"][0](combined)
-        for i in range(1, len(self.lyr["lyr"])):
-            result = self.lyr["lyr"][i](result)
+        result = self.lyr[0](inputs)
+        for i in range(1, len(self.lyr)):
+            result = self.lyr[i](result)
 
         return result
 
@@ -148,16 +133,11 @@ if gpus:
 pyplot.figure(figsize=(4,2))
 
 if(TRAIN == True):
-    trainingSet = ImageConverter.ImageConverter(os.path.dirname(__file__) + "\images_training", BATCH_SIZE, BATCH_SIZE)
-    trainingImages, trainingLabels = trainingSet.process(True)
- 
-    pyplot.subplot(4,8,1)
-    pyplot.imshow(trainingImages[0])
-    pyplot.subplot(4,8,2)
-    pyplot.imshow(trainingLabels[0])
+    trainingImages = SineWave.Sin_Data()
+    trainingLabels = SineWave.Sin_Label()
 
-    testSet = ImageConverter.ImageConverter(os.path.dirname(__file__) + "\images_test", BATCH_SIZE, BATCH_SIZE)
-    testImages, testLabels = testSet.process(True)
+    testImages = SineWave.Sin_Data()
+    testLabels = SineWave.Sin_Label()
 
 # beef = tensorflow.concat([1, 2], 0)
 
@@ -168,8 +148,7 @@ model = MyModelV2()
 checkpoint_path = "data/checkpoint.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path) 
 
-predictSet = ImageConverter.ImageConverter(os.path.dirname(__file__) + "\images", BATCH_SIZE, BATCH_SIZE)
-predictImages, junk = predictSet.process(True)
+predictImages = SineWave.Sin_Data()
 
 if(TRAIN == True):
     logger.log("Train", "Training Start!")
@@ -191,27 +170,9 @@ else:
     model.compile(optimizer='adam',
               loss=tensorflow.keras.losses.Huber(),
               metrics=["accuracy"])
-    model.build((1, 32, 32, 4))
+    model.build((1, 1, 1, 28))
 
 # logger.log("MODEL SUMMARY", str(model.summary()))
 
 prediction = model.predict(predictImages, steps=42)
-
-#model.getResult(4, 8, 9, predictImages)
-
-#pIndex = 0
-
-#pyplot.subplot(4,8,22)
-#pyplot.imshow(predictImages[0])
-
-pyplot.subplot(4,8,23)
-pyplot.imshow(prediction[0])
-
-minVal = numpy.min(prediction[0])
-maxVal = numpy.max(prediction[0])
-
-scaled = (prediction[0] - minVal) / (maxVal - minVal)
-
-matplotlib.image.imsave("target.png", scaled)
-
-pyplot.show()
+print(prediction)
